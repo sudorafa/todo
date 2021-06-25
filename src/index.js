255a107c7a829429c3568162243048299ec38fd7
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2021-06-25 19:54:56 -0300

    componente footer e estrutura para screen home

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import clsx from 'clsx';
+import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
 import AppBarApp from './components/appBar'
 import menus from './constants/menu'
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Copyright from './components/footer'
+import Home from './screen/home'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -64,8 +48,6 @@ const getTitleMenu = (menu) => {
 export default function App() {
   const classes = useStyles();
   const [menu, setMenu] = React.useState(menus.TODO);
-  
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   const handleMenu = menu => {
     setMenu(menu);
@@ -78,37 +60,17 @@ export default function App() {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         { menu === menus.TODO &&
-          <Container maxWidth="lg" className={classes.container}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper className={fixedHeightPaper}>
-                  
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper className={fixedHeightPaper}>
-                  
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper className={classes.paper}>
-                  
-                </Paper>
-              </Grid>
-            </Grid>
-            <Box pt={4}>
-              <Copyright />
-            </Box>
-          </Container>
+          <Home />
         }
         { menu === menus.ABOUT &&
           <Container maxWidth="lg" className={classes.container}>
             SOBRE
           </Container>
         }
+
+        <Box pt={4}>
+          <Copyright />
+        </Box>
         </main>
     </div>
   );
